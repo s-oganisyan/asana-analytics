3c5c9402295e495e72141878c5e6a5de63630cdd
andrey-mozhaev <a.mozhaev.exceedteam@gmail.com>
2021-01-28 12:46:49 +0300

    fix app.ts

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,6 @@ import express from 'express';
 import Logger from './logger';
 import config from './config';
 import loaders from './loaders';
-import CsvService from './services/gcsServices/gcsService';
 
 async function startServer(): Promise<void> {
   const app = express();
@@ -11,7 +10,6 @@ async function startServer(): Promise<void> {
   } catch (e) {
     Logger.error(e);
   }
-  await new CsvService().loadCsvInGcs('test.csv', 'dev_data/test.csv');
   app.listen(config.PORT, () => {
     Logger.info(`port: ${config.PORT}`);
   });
