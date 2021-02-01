2b6b4e59f366d465e3f1d546a6a3075c376dbd7e
andrey-mozhaev <a.mozhaev.exceedteam@gmail.com>
2021-02-01 13:52:47 +0300

    add gbq in health check

--- src/api/routers/helthCheck.ts
+++ src/api/routers/helthCheck.ts
@@ -1,4 +1,6 @@
 import config from '../../config';
+import Logger from '../../logger';
+import gbq from '../../loaders/googleBigquery';
 import gcs from '../../loaders/googleCloudStorage';
 import { Router } from 'express';
 const router = Router();
@@ -7,18 +9,28 @@ const router = Router();
 export default (app: Router): void => {
   app.use('/', router);
   router.get('/', async (req, res) => {
-    let googleCloudStorage: boolean;
+    let googleCloudStorage = false;
+    let googleBigquery = false;
+
+    try {
+      await gbq().getDatasets();
+      googleBigquery = true;
+    } catch (e) {
+      Logger.error(e);
+    }
 
     try {
       const bucket = await gcs().bucket(config.GCS.PROJECT_NAME);
       await bucket.get();
       googleCloudStorage = true;
     } catch (e) {
-      googleCloudStorage = false;
+      Logger.error(e);
     }
+
     res.status(200).send({
       node: true,
       googleCloudStorage,
+      googleBigquery,
     });
   });
 };
