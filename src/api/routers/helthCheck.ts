21d0d8e5324604c46a73da15e3608c4249975efa
andrey-mozhaev <a.mozhaev.exceedteam@gmail.com>
2021-02-01 13:09:22 +0300

    add gcs in health check

--- src/api/routers/helthCheck.ts
+++ src/api/routers/helthCheck.ts
@@ -7,10 +7,18 @@ const router = Router();
 export default (app: Router): void => {
   app.use('/', router);
   router.get('/', async (req, res) => {
-    const bucket = await gcs().bucket(config.GCS.PROJECT_NAME);
+    let googleCloudStorage: boolean;
+
+    try {
+      const bucket = await gcs().bucket(config.GCS.PROJECT_NAME);
+      await bucket.get();
+      googleCloudStorage = true;
+    } catch (e) {
+      googleCloudStorage = false;
+    }
     res.status(200).send({
       node: true,
-      googleCloudStorage: !!bucket.getId(),
+      googleCloudStorage,
     });
   });
 };
